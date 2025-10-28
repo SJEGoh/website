d4264c88bfd767c0070fb18a11b8175c93c4a5b2
SJEGoh <57899746+SJEGoh@users.noreply.github.com>
2025-10-28 22:54:55 +0800

    Form now works :)

--- app.py
+++ app.py
@@ -6,8 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
         
-@app.route('/contact')
+@app.route('/contact', methods = ['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        name = request.form.get("name")
+        email = request.form.get("email")
+        purpose = request.form.get("purpose_of_contact")
+        message = request.form.get("user_message")
+        print(name, email, purpose, message)
     return render_template('contact.html')
 
 
